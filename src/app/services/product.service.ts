d3f81fcb6d90dd4f89966819689a2dcf41781b99
devturakhiya2006 <dev.turakhiya2006@gmail.com>
2026-07-30 14:16:28 +0530

    api get file create

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private http: HttpClient) { }
+
+  getAllProduct() {
+    return this.http.get("https://dummyjson.com/products").subscribe((data)=>{
+    console.log(data)
+    })
+  }
+}
+
+
